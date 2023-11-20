700a9050d2703c3767449ad1b18fa4efceb17c7b
sanxxtiago <csriosv@correo.udistrital.edu.co>
2023-11-20 14:41:31 -0500

    Conexión con la bd
    Conexión a la base de datos alojada en la nube

--- src/sistemamensajeria/SistemaMensajeria.java
+++ src/sistemamensajeria/SistemaMensajeria.java
@@ -4,17 +4,30 @@
  */
 package sistemamensajeria;
 
+import conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author Santiago
  */
 public class SistemaMensajeria {
-
+    
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        
+        //Establece la conexión con la bd alojada en la nube
+        Conexion.getSingleton().establecerConexion();
+        
     }
+    
     
 }
