b2cb612c8524312ccd1e4ef7f3a0b93b54f93b60
sofia <93295824+sofiaya1a@users.noreply.github.com>
2023-11-28 16:45:12 -0500

    Bien comentado

--- src/dao/ServicioDAO.java
+++ src/dao/ServicioDAO.java
@@ -13,22 +13,30 @@ import util.ServiceLocator;
 
 public class ServicioDAO {
 
-    public ServicioDAO(){
+    // Constructor Servicio
+    public ServicioDAO() {
+        // Constructor vacío por ahora
     }
 
+    // Método para consultar servicios por estado
     public List<Servicio> consultarServiciosPorEstado(String estado) throws RHException {
         List<Servicio> servicios = new ArrayList<>();
 
         try {
+            // Consulta SQL para seleccionar servicios por estado
             String strSQL = "SELECT * FROM public.servicio WHERE estado = ?";
+            
+            // Obtener la conexión desde el ServiceLocator
             Connection conexion = ServiceLocator.getInstance().tomarConexion();
+            
+            // PS para ejecutar la consulta
             PreparedStatement prepStmt = conexion.prepareStatement(strSQL);
             prepStmt.setString(1, estado);
 
             ResultSet rs = prepStmt.executeQuery();
 
+            // Itera sobre los resultados y los "convierte" a objetos Servicio
             while (rs.next()) {
-                // Mapear los resultados a objetos Servicio y agregarlos a la lista
                 Servicio servicio = new Servicio();
                 servicio.setIdServicio(rs.getInt("idServicio"));
                 servicio.setTipoIdCliente(rs.getString("tipoIdCliente"));
@@ -45,27 +53,37 @@ public class ServicioDAO {
             }
             rs.close();
             prepStmt.close();
+
         } catch (SQLException e) {
             throw new RHException("ServicioDAO", "Error al consultar servicios por estado: " + e.getMessage());
         } finally {
-            ServiceLocator.getInstance().liberarConexion();
+            ServiceLocator.getInstance().rollback();
         }
-        return servicios;
-    }   
 
+        // Devolver la lista de servicios
+        return servicios;
+    }
+
+    // Método para consultar servicios por ID de cliente
     public List<Servicio> consultarServiciosPorIdCliente(int idCliente) throws RHException {
         List<Servicio> servicios = new ArrayList<>();
 
         try {
+            // Consulta SQL para seleccionar servicios por ID de cliente
             String strSQL = "SELECT * FROM public.servicio WHERE idCliente = ?";
+            
+            // Obtener la conexión desde el ServiceLocator
             Connection conexion = ServiceLocator.getInstance().tomarConexion();
+            
+            // PS preparada para ejecutar la consulta
             PreparedStatement prepStmt = conexion.prepareStatement(strSQL);
             prepStmt.setInt(1, idCliente);
 
+            // Ejecutar la consulta 
             ResultSet rs = prepStmt.executeQuery();
 
+            // Itera sobre los resultados y los "convierte" a objetos Servicio
             while (rs.next()) {
-                // Mapear los resultados a objetos Servicio y agregarlos a la lista
                 Servicio servicio = new Servicio();
                 servicio.setIdServicio(rs.getInt("idServicio"));
                 servicio.setTipoIdCliente(rs.getString("tipoIdCliente"));
@@ -80,15 +98,17 @@ public class ServicioDAO {
 
                 servicios.add(servicio);
             }
-
+            // Cierre
             rs.close();
             prepStmt.close();
+
         } catch (SQLException e) {
             throw new RHException("ServicioDAO", "Error al consultar servicios por ID de cliente: " + e.getMessage());
         } finally {
-            ServiceLocator.getInstance().liberarConexion();
-        }
 
+            ServiceLocator.getInstance().rollback();
+        }
+        // Devolver la lista de servicios
         return servicios;
     }
 }
